93fbf5174fc996d47729fb4273c0f136ccd997bb
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-10-30 17:12:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ ISO_PATH="/opt/iso/winpe.iso"   # Path to the ISO file
 EXTRACT_DIR="/opt/iso/winpe"    # Directory to extract the ISO contents
 MAIN_SCRIPT_PATH="/usr/local/bin/usb_writer.sh"  # Path where the main script will be installed
 GIT_REPO="https://github.com/AWT-Trent/NYX-RE.git"  # Git repository URL
-VERSION_FILE="version.txt"
-URL_FILE="download_url.txt"
+VERSION_FILE="/tmp/NYX-RE/version.txt"
+URL_FILE="/tmp/NYX-RE/download_url.txt"
 TEMP_ISO="/opt/iso/nyx_temp.iso"
 
 # Ensure the script is being run as root
