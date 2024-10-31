a05a70941bc551119586efa136e9ad7b3c492571
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-10-31 11:40:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,40 @@ VERSION_FILE="/tmp/NYX-RE/version.txt"
 URL_FILE="/tmp/NYX-RE/download_url.txt"
 TEMP_ISO="/opt/iso/nyx_temp.iso"
 EXPECTED_PATH="/tmp/NYX-RE/setup.sh"
-CURRENT_VERSION=$(cat "/tmp/NYX-RE/version.txt")
+
+
+
+download_and_extract_iso() {
+    DOWNLOAD_URL=$(cat "$URL_FILE")
+
+    # Clean the download URL by removing everything after the '?' and adding "download=1"
+    CLEANED_URL=$(echo "$DOWNLOAD_URL" | sed 's/\?.*/?download=1/')
+
+    echo "Downloading ISO from cleaned URL: $CLEANED_URL"
+
+    # Download the ISO to a temporary file
+    wget -O "$TEMP_ISO" "$CLEANED_URL"
+
+    if [ $? -eq 0 ]; then
+        echo "Download successful, moving to $ISO_PATH"
+        mv "$TEMP_ISO" "$ISO_PATH"
+    else
+        echo "Error downloading the ISO."
+        exit 1
+    fi
+    # Extract the ISO contents
+    echo "Extracting ISO contents to $EXTRACT_DIR..."
+    if [ -d "$EXTRACT_DIR" ]; then
+        echo "Removing old extracted files..."
+        sudo rm -rf "$EXTRACT_DIR"
+    fi
+    sudo mkdir -p "$EXTRACT_DIR"
+    sudo 7z x "$ISO_PATH" -o"$EXTRACT_DIR"
+    echo "ISO extraction complete."
+
+
+}
+
 
 # Ensure the script is being run as root
 if [ "$EUID" -ne 0 ]; then
@@ -17,19 +50,26 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-# Clear the crontab and add a reboot task to start usb_writer.sh
-echo "Clearing the crontab..."
-crontab -r
 
-echo "Writing crontab entry to start the writer script on reboot..."
-(crontab -l 2>/dev/null; echo "@reboot sudo $MAIN_SCRIPT_PATH") | crontab -
+if [ -f "$VERSION_FILE" ]; then
+    CURRENT_VERSION=$(cat "/tmp/NYX-RE/version.txt")
+else
+    download_and_extract_iso
+
 
 # Update package lists and install necessary dependencies
 echo "Updating package lists..."
 sudo apt-get update -y
 
 echo "Installing required packages..."
-sudo apt-get install -y util-linux parted dosfstools p7zip-full git
+sudo apt-get install -y util-linux parted dosfstools p7zip-full git cron
+
+# Clear the crontab and add a reboot task to start usb_writer.sh
+echo "Clearing the crontab..."
+crontab -r
+
+echo "Writing crontab entry to start the writer script on reboot..."
+(crontab -l 2>/dev/null; echo "@reboot sudo $MAIN_SCRIPT_PATH") | crontab -
 
 # Clone the latest repository to ensure we're using the most up-to-date scripts
 echo "Cloning the latest version of the repository..."
@@ -72,33 +112,8 @@ fi
 echo "Checking for update"
 if [ -f "$VERSION_FILE" ] && [ -f "$URL_FILE" ]; then
     NEW_VERSION=$(cat "$VERSION_FILE")
-    if [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then    
-        DOWNLOAD_URL=$(cat "$URL_FILE")
-
-        # Clean the download URL by removing everything after the '?' and adding "download=1"
-        CLEANED_URL=$(echo "$DOWNLOAD_URL" | sed 's/\?.*/?download=1/')
-
-        echo "Downloading ISO from cleaned URL: $CLEANED_URL"
-
-        # Download the ISO to a temporary file
-        wget -O "$TEMP_ISO" "$CLEANED_URL"
-
-        if [ $? -eq 0 ]; then
-            echo "Download successful, moving to $ISO_PATH"
-            mv "$TEMP_ISO" "$ISO_PATH"
-        else
-            echo "Error downloading the ISO."
-            exit 1
-        fi
-        # Extract the ISO contents
-        echo "Extracting ISO contents to $EXTRACT_DIR..."
-        if [ -d "$EXTRACT_DIR" ]; then
-            echo "Removing old extracted files..."
-            sudo rm -rf "$EXTRACT_DIR"
-        fi
-        sudo mkdir -p "$EXTRACT_DIR"
-        sudo 7z x "$ISO_PATH" -o"$EXTRACT_DIR"
-        echo "ISO extraction complete."
+    if [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then
+        download_and_extract_iso    
     fi
 else
     echo "Error: Version or URL file missing."
